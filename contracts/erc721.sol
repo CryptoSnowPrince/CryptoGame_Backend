aa41c942c8c777f7a6f51384482e7a59efe3a06d
DESKTOP-3J65I2E\admin <RogerDTilton@gmail.com>
2021-12-17 08:20:51 +0900

    add erc721.sol, Ownable.sol, SafeMath.sol

---
+++ contracts/erc721.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT License
+pragma solidity 0.8.10;
+
+/// @dev OpenZeppelin's implementation of the ERC721 spec draft
+abstract contract ERC721 {
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 indexed _tokenId
+  );
+  event Approval(
+    address indexed _owner,
+    address indexed _approved,
+    uint256 indexed _tokenId
+  );
+
+  function balanceOf(address _owner) external view virtual returns (uint256);
+
+  function ownerOf(uint256 _tokenId) external view virtual returns (address);
+
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId
+  ) external payable virtual;
+
+  function approve(address _approved, uint256 _tokenId)
+    external
+    payable
+    virtual;
+}
